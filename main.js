27ab48b0bdc144b6e00efee23b0c9793c17d2a16
duc810 <duc81090@gmail.com>
2017-07-20 09:52:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -46,10 +46,7 @@ socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
 
 //hiện thị trình video lên
 function openStream() {
-    const config = { audio: {
-      echoCancellation: { exact: false },
-      deviceId: audioSource
-    }, video: true };
+    const config = { audio: true, video: true };
   //console.log(navigator.mediaDevices.getUserMedia(config));return;
     return navigator.mediaDevices.getUserMedia(config);
 }
@@ -101,6 +98,40 @@ peer.on('call', call => {
         call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
     });
 });
+
+
+
+
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constraints) {
+
+    // First get ahold of the legacy getUserMedia, if present
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  }
+}
+
+
+
+
 
 //click để call
 $('#ulUser').on('click', 'li', function() {
